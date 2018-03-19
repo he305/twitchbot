ddf03609ba6702a2295b899235ba22181cb1e491
he305 <he305@mail.ru>
2018-03-20 02:38:54 +0300

    plot

--- analysis.py
+++ analysis.py
@@ -3,32 +3,137 @@ import glob
 from collections import Counter
 import requests
 import pprint
-from cfg import emotes_global, emotes_bttv_global, CLIENT_ID
-from emotes import emotes_channels
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from emotes import emotes_channels, emotes_bttv_global, emotes_global
+import datetime
+import math
 
+
+class Message:
+    def __init__(self, nickname, time, message):
+        self.nickname = nickname
+        self.time = time
+        self.message = message
+
+        self.split_message()
+    
+    def split_message(self):
+        self.words = self.message.split()
+
+        global emotes_all
+        self.emotes = []
+        for word in self.words:
+            if word in emotes_all:
+                self.emotes.append(word)
+            for emote in emotes_channels:
+                if emote in word:
+                    self.emotes.append(word)
+        
+        self.words = [word.lower() for word in self.words if word not in self.emotes and len(word) >= 4]
+
+        while "@" + channel in self.emotes: 
+            self.emotes.remove("@" + channel)
+
+        while "@" + channel in self.words:
+            self.words.remove("@" + channel)
+
+
+    def __lt__(self, other):
+         return self.time < other.time
+
+
+    def count_emote(self, emote):
+        return self.emotes.count(emote)
+            
+
+emotes_all = []
+channel = ""
 
 def get_bttv_local(channel):
+    files = glob.glob("emotes_channels\\*.txt")
+    if "emotes_channels\\{}.txt".format(channel+'_bttv') in files:
+        with open("emotes_channels\\{}.txt".format(channel+'_bttv'), encoding="utf8") as f:
+            data = f.readlines()
+        return data
+
     emotes = requests.get("https://api.betterttv.net/2/channels/" + channel).json()
     if not 'emotes' in emotes:
         return [""]
     codes = [emote['code'] for emote in emotes['emotes']]
+
+    with open("emotes_channels\\{}.txt".format(channel+'_bttv'), 'w+', encoding="utf8") as f:
+        for code in codes:
+            f.write('"{}"\n'.format(code))
+
     return codes
 
 
 def get_ffz_local(channel):
+    files = glob.glob("emotes_channels\\*.txt")
+    if "emotes_channels\\{}.txt".format(channel+'_ffz') in files:
+        with open("emotes_channels\\{}.txt".format(channel+'_ffz'), encoding="utf8") as f:
+            data = f.readlines()
+        return data
+
     emotes = requests.get("https://api.frankerfacez.com/v1/room/" + channel).json()
-    if not 'sets' in emotes:
+    if not 'sets' in emotes or len(emotes['sets'][str(list(emotes['sets'].keys())[0])]['emoticons']) == 0:
         return [""]
-    
-    
+
     emotes_data = emotes['sets'][str(list(emotes['sets'].keys())[0])]['emoticons']
     codes = [emote['name'] for emote in emotes_data]
+
+    with open("emotes_channels\\{}.txt".format(channel+'_ffz'), 'w+', encoding="utf8") as f:
+        for code in codes:
+            f.write('"{}"\n'.format(code))
+
     return codes
 
 
 def print_files(files):
     for txt in files:
         print(txt.split("\\")[1])
+
+
+def get_dynamic(emote, messages):
+    sorted_messages = sorted(messages, key=lambda message: message.time)
+    emote_sum = 0
+    x = []
+    y = []
+    start_time = messages[0].time
+    prev_count = 0
+    i = 0
+    interval = math.floor(len(messages)/200)
+    for msg in sorted_messages:
+        emote_sum += msg.count_emote(emote)
+        i += 1
+        if i >= interval:
+            if len(x) == 0:
+                time = 0
+            else:
+                time = (msg.time - start_time)/datetime.timedelta(minutes=1)
+
+            ###TEMPORARY###
+            if len(x) > 1 and msg.time == x[-1]:
+                continue
+            ###############
+            if sys.argv[4] == "delta":
+                delta = emote_sum - prev_count
+                prev_count = emote_sum
+                y.append(delta)   
+            elif sys.argv[4] == "graph":
+                y.append(emote_sum)
+            
+            x.append(time)
+            i = 0
+    # data
+    df=pd.DataFrame({'x': x, 'y': y})
+    plt.title(emote)
+    # plot
+    plt.plot( 'x', 'y', data=df, linestyle='-')
+    plt.show()
+
 
 
 def main():
@@ -38,6 +143,7 @@ def main():
         print_files(files)
         return
 
+    global channel
     channel = sys.argv[1]
     if "channels\\{}.txt".format(channel) not in files:
         print("Wrong channel, available are: \n", "-"*10)
@@ -47,36 +153,50 @@ def main():
     with open("channels\\{}.txt".format(channel), encoding="utf8") as f:
         data = f.readlines()
 
-    emotes_all = emotes_global + emotes_bttv_global + get_bttv_local(channel) + get_ffz_local(channel)
 
-    nicknames = []
+    global emotes_all
+    emotes_all = emotes_global + emotes_bttv_global + get_bttv_local(channel) + get_ffz_local(channel)    
+
     messages = []
     words = []
     emotes = []
+    nicknames = []
+
+
     for msg in data:
         if msg == '\n':
             continue
-        nicknames.append(msg.split(':')[0])
-        messages.append(msg.split(':')[1].strip())
+
+        ###TEMPORARY
+        data_msg = msg.split(':')
+        if len(data_msg) == 2 or len(data_msg[0]) != 19:
+            nickname = data_msg[0]
+            message = data_msg[1].strip()
+            time = datetime.datetime.strptime("2018-03-19 20-00-00", "%Y-%m-%d %H-%M-%S")
+        else:
+            time = datetime.datetime.strptime(data_msg[0], "%Y-%m-%d %H-%M-%S")
+            nickname = data_msg[1]
+            message = data_msg[2].strip()
+        
+        messages.append(Message(nickname, time, message))
+
 
     for msg in messages:
-        for spl in msg.split():
-            words.append(spl)
-            if spl in emotes_all:
-                emotes.append(spl)
+        words += msg.words
+        emotes += msg.emotes
+        nicknames.append(msg.nickname)
 
-            for emote in emotes_channels:
-                if emote in spl:
-                    emotes.append(spl)
-
-    while "@" + channel in emotes: 
-        emotes.remove("@" + channel)
 
     
-    if sys.argv[2] == "emote" or sys.argv[2] == "full":
-        print("Top {} channel emotes\n".format(sys.argv[3]), "-"*20)
+    if sys.argv[2] == "emotes" or sys.argv[2] == "full":
+        if len(sys.argv) == 3:
+            top = 20
+        else:
+            top = sys.argv[3]
+
+        print("Top {} channel emotes\n".format(top), "-"*20)
         c = Counter(emotes)
-        pprint.pprint(c.most_common(int(sys.argv[3])))
+        pprint.pprint(c.most_common(int(top)))
         print("-"*20)
 
     if sys.argv[2] == "nick":
@@ -91,10 +211,18 @@ def main():
         print("-"*20)
 
     if sys.argv[2] == "words":
-        print("Top {} most used words\n".format(sys.argv[3]), "-"*20)
+        if len(sys.argv) == 3:
+            top = 20
+        else:
+            top = sys.argv[3]
+
+        print("Top {} most used words\n".format(top, "-"*20))
         c = Counter(words)
-        pprint.pprint(c.most_common(int(sys.argv[3])))
+        pprint.pprint(c.most_common(int(top)))
         print("-"*20)
+    
+    if sys.argv[2] == "emote":
+        get_dynamic(sys.argv[3], messages)
 
 if __name__ == "__main__": 
     main()
--- bot.py
+++ bot.py
@@ -5,31 +5,77 @@ import socket
 import re
 import threading
 from time import sleep
+import datetime
+import requests
+from cfg import CLIENT_ID
 
 messages = []
 running = False
 
 def main():
-    global running
     if len(sys.argv) == 1:
         print("Input channel name")
         return 
     
+    headers = {
+        'Client-ID' : CLIENT_ID,
+        'Accept' : 'application/vnd.twitchtv.v5+json'
+    }
+
+    data = requests.get("https://api.twitch.tv/kraken/users?login=" + sys.argv[1], headers=headers).json()
+    
+    global streamer_id
+    streamer_id = data['users'][0]['_id']
+    
+    stream_data = requests.get("https://api.twitch.tv/kraken/streams/" + streamer_id, headers=headers).json()
+    
+    
+    
+    global ignore_offline
+
+    if stream_data['stream'] is None:
+        print("Stream is offline, continue anyway Y/N")
+        if input() == "N":
+            return
+        ignore_offline = True
+
+    global running
     running = True
     chat_thread.start()
     save_thread.start()
+
     while True:
         if input() == "exit":
             running = False
+
+        if running == False:
             print("Stopping the bot")
             chat_thread.join()
             print("Chat thread is stopped")
             save_thread.join()
             print("Saving thread is stopped")
             save()
+            if (check_online_thread.is_alive())
+                check_online_thread.join()
             print("Quiting...")
             sys.exit()
 
+
+def check_online():
+    headers = {
+        'Client-ID' : CLIENT_ID,
+        'Accept' : 'application/vnd.twitchtv.v5+json'
+    }
+
+    global streamer_id
+    global running
+    while True:
+        stream_data = requests.get("https://api.twitch.tv/kraken/streams/" + streamer_id, headers=headers).json()
+        if stream_data['stream'] is None:
+            print("Streamer has gone offline, finishing...")
+            running = False
+        sleep(60)
+        
 
 def read_chat():
     cfg.CHANNEL = sys.argv[1]
@@ -42,20 +88,30 @@ def read_chat():
     ###Thread for writing messages to the file every [arg] seconds
 
     chat_message = re.compile(r":?\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+
+    if not ignore_offline:
+        check_online_thread.start()
     while running:
-        response = s.recv(2048).decode("utf-8")
-        if response == "PING :tmi.twitch.tv\r\n":
-            s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
-        else:
-            data = response.splitlines()
-            for res in data:
-                message = chat_message.sub("", res).strip()
-                if re.search(r"\w+", res) is not None:
-                    full_message = "{0}: {1}".format(re.search(r"\w+", res).group(0), message)
-                    print(full_message)
-                    
-                    if not "tmi" in full_message:
-                        messages.append("{0}: {1}".format(re.search(r"\w+", res).group(0), message))
+        try:
+            response = s.recv(1024).decode("utf-8")
+            if response == "PING :tmi.twitch.tv\r\n":
+                s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+            else:
+                data = response.splitlines()
+                for res in data:
+                    message = chat_message.sub("", res).strip()
+                    if re.search(r"\w+", res) is not None:
+                        full_message = "{0}: {1}".format(re.search(r"\w+", res).group(0), message)
+                        print(full_message)
+                        
+                        if not "tmi" in full_message:
+                            time = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+                            messages.append("{0}:{1}: {2}".format(time, re.search(r"\w+", res).group(0), message))
+        except UnicodeDecodeError as ex:
+            print('*'*20)
+            print(ex)
+            response = 0
+            print('*'*20)
         sleep(0.1)
 
 
@@ -81,8 +137,11 @@ def save_messages(sleep_time):
 
         sleep(sleep_time)
 
-
+streamer_id = 0
 chat_thread = threading.Thread(target=read_chat, )
 save_thread = threading.Thread(target=save_messages, args=(cfg.SAVE_TIME, ))
+check_online_thread = threading.Thread(target=check_online, )
+ignore_offline = False
+
 if __name__ == "__main__":
     main()
